a31b03d3c924d36fa01c7b816e849c7e1f48f47d
catg2265 <cgrave20@student.aau.dk>
2023-03-31 15:17:03 +0200

    fpaoksjføpsdikjnf

---
+++ MyApplication/app/src/main/java/com/example/myapplication/CalculatorActivity.java
@@ -0,0 +1,102 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener
+{
+	EditText num1text;
+	EditText num2text;
+	TextView resultText;
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_calculator);
+		num1text   = findViewById(R.id.number1);
+		num2text   = findViewById(R.id.number2);
+		resultText = findViewById(R.id.result);
+	}
+
+	@Override
+	public void onClick(View view)
+	{
+
+		int num1 = Integer.parseInt(num1text.getText().toString());
+		int num2 = Integer.parseInt(num2text.getText().toString());
+		int result;
+
+		if (view.getId() == R.id.button)
+		{
+			// Addition
+			result = num1 + num2;
+			Log.d("plus", "onClick: plus");
+			resultText.setText(String.valueOf(result));
+		}
+		if (view.getId() == R.id.button2)
+		{
+			// Subtraction
+			result = num1-num2;
+			Log.d("minus", "onClick: minus");
+			resultText.setText(String.valueOf(result));
+		}
+		if (view.getId() == R.id.button3)
+		{
+			// Multiplication
+			result = num1*num2;
+			Log.d("multiply", "onClick: multiply");
+			resultText.setText(String.valueOf(result));
+		}
+		if (view.getId() == R.id.button4)
+		{
+			// Division
+			result = num1/num2;
+			Log.d("divide", "onClick: divide");
+			resultText.setText(String.valueOf(result));
+		}
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater menuInflater = getMenuInflater();
+
+		menuInflater.inflate(R.menu.mymenu, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+	@SuppressLint("NonConstantResourceId")
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case R.id.home:
+				Intent intent = new Intent(this, MainActivity.class);
+				startActivity(intent);
+				break;
+			case R.id.secondlayout:
+				setContentView(R.layout.activity_secondary);
+				break;
+			case R.id.inputlayout:
+				Intent intent2 = new Intent(this, CalculatorActivity.class);
+				startActivity(intent2);
+				break;
+			case R.id.exit:
+				finish();
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+}
+
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,24 +3,19 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import android.widget.EditText;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
 	TextView text1;
-	TextView warning = findViewById(R.id.textView2);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -45,31 +40,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		{
 			CounterLayout2();
 		}
-		/*if (view.getId() == R.id.button)
-		{
-			Calculator(view);
-		}
-		if (view.getId() == R.id.button2)
-		{
-			Calculator(view);
-		}
-		if (view.getId() == R.id.button3)
-		{
-			Calculator(view);
-		}
-		if (view.getId() == R.id.button4)
-		{
-			Calculator(view);
-		}*/
 	}
+	@SuppressLint("SetTextI18n")
 	private void CounterLayout2()
 	{
 		TextView textCounter = findViewById(R.id.txtCount);
 		String currentText = textCounter.getText().toString();
-		Integer current = Integer.parseInt(currentText);
+		int    current     = Integer.parseInt(currentText);
 		current++;
-		Log.d("current count", current.toString());
-		textCounter.setText(current.toString());
+		Log.d("current count", Integer.toString(current));
+		textCounter.setText(Integer.toString(current));
 	}
 	public void GoToSecondaryLayout(View view)
 	{
@@ -102,8 +82,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 				setContentView(R.layout.activity_secondary);
 				break;
 			case R.id.inputlayout:
-				setContentView(R.layout.activity_input);
-				warning.setVisibility(View.INVISIBLE);
+				Intent intent = new Intent(this, CalculatorActivity.class);
+				startActivity(intent);
 				break;
 			case R.id.exit:
 				finish();
@@ -111,53 +91,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 		return super.onOptionsItemSelected(item);
 	}
-	public void Calculator(View view)
-	{
-		EditText num1text = findViewById(R.id.number1);
-		EditText num2text = findViewById(R.id.number2);
-		TextView resultText = findViewById(R.id.result);
-		Integer result;
-
-		boolean isDigitOnly1 = TextUtils.isDigitsOnly(num1text.getText());
-		boolean isDigitOnly2 = TextUtils.isDigitsOnly(num2text.getText());
-
-		if (isDigitOnly1 && isDigitOnly2)
-		{
-			Integer num1 = Integer.parseInt(String.valueOf(num1text.getText()));
-			Integer num2 = Integer.parseInt(String.valueOf(num2text.getText()));
-			if (view.getId() == R.id.button)
-			{
-				// Addition
-				result = num1 + num2;
-				resultText.setText(result.toString());
-				warning.setVisibility(View.INVISIBLE);
-			}
-			if (view.getId() == R.id.button2)
-			{
-				// Subtraction
-				result = num1 - num2;
-				resultText.setText(result.toString());
-				warning.setVisibility(View.INVISIBLE);
-			}
-			if (view.getId() == R.id.button3)
-			{
-				// Multiplication
-				result = num1 * num2;
-				resultText.setText(result.toString());
-				warning.setVisibility(View.INVISIBLE);
-			}
-			if (view.getId() == R.id.button4)
-			{
-				// Division
-				result = num1 / num2;
-				resultText.setText(result.toString());
-				warning.setVisibility(View.INVISIBLE);
-			}
-		}else
-		{
-			warning.setVisibility(View.VISIBLE);
-		}
 
 
-	}
 }
